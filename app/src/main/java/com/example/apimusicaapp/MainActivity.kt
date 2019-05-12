658850842f1d9b0139aba253308bd09d7eb4ecfa
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-05-12 16:16:54 -0600

    Guarda todos los datos en la base de datos!! Ahora a leer los datos de la base y no de Internet.

--- app/src/main/java/com/example/apimusicaapp/MainActivity.kt
+++ app/src/main/java/com/example/apimusicaapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.apimusicaapp
 
+import android.content.ContentValues
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.AsyncTask
@@ -7,9 +8,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
+import android.widget.Toast
 import com.example.apimusicaapp.Fragmento.FragmentoInfoSong
 import com.example.apimusicaapp.adapter.Adapter
 import com.example.apimusicaapp.adapter.Viewer
+import com.example.apimusicaapp.data.Database
+import com.example.apimusicaapp.data.DatabaseContract
 import com.example.apimusicaapp.model.Cancion
 import com.example.apimusicaapp.utils.NetworkUtils
 import com.example.apimusicaapp.utils.SongSerializer
@@ -17,6 +21,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity(), FragmentoInfoSong.OnFragmentInteractionListener {
+    var dbHelper=Database(this)
     override fun landscape(song: Cancion) {
         var fragmento= FragmentoInfoSong()
         supportFragmentManager.beginTransaction().replace(R.id.land_segundo,fragmento).commit()
@@ -66,12 +71,35 @@ class MainActivity : AppCompatActivity(), FragmentoInfoSong.OnFragmentInteractio
             }
         }
         override fun onPostExecute(result: List<Cancion>?) {
-            if (result != null) {
-                if (result.isNotEmpty()) {
-                    viewAdapter.setData(result)
-                } else {
-                    Log.d("Holi", "No se pudo encontrar los datos")
-                }
+            listCancion(result)
+        }
+    }
+    fun listCancion(respuesta:List<Cancion>?){
+        if (respuesta != null) {
+            /*val cancionLista = */if (respuesta.isNotEmpty()) {
+                viewAdapter.setData(respuesta)
+            } else {
+                Log.d("Holi", "No se pudo encontrar los datos")
+            }
+            meterBaseDatos(respuesta)
+        }
+    }
+    private fun meterBaseDatos(lista:List<Cancion>){
+        val db = dbHelper.writableDatabase
+        dbHelper.check(db)
+        for(i in 0..7){
+            val values=ContentValues().apply {
+                put(DatabaseContract.CancionEntry.COLUMN_NAME,lista[i].name)
+                put(DatabaseContract.CancionEntry.COLUMN_ARTISTA,lista[i].artista)
+                put(DatabaseContract.CancionEntry.COLUMN_ANIO,lista[i].anio)
+                put(DatabaseContract.CancionEntry.COLUMN_ALBUM,lista[i].album)
+            }
+            val newRowId=db?.insert(DatabaseContract.CancionEntry.TABLE_NAME,null,values)
+            if (newRowId == -1L) {
+                Log.d("Hola","NO SE HA GUARDADO")
+            } else {
+                //Toast.makeText(this, "Guardado con exito", Toast.LENGTH_SHORT).show()
+                Log.d("Hola", "Guardado con exito")
             }
         }
     }
