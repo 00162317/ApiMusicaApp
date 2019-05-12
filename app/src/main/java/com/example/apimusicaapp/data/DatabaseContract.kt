babf89d6df96ce761b7c7e1209477fbd8d5e9a29
Roberto Hernandez <hernandez.roberto014@gmail.com>
2019-05-12 15:41:02 -0600

    Definiendo el nombre de la tabla y las columnas

--- app/src/main/java/com/example/apimusicaapp/data/DatabaseContract.kt
+++ app/src/main/java/com/example/apimusicaapp/data/DatabaseContract.kt
@@ -1,4 +1,16 @@
 package com.example.apimusicaapp.data
 
+import android.provider.BaseColumns
+
 object DatabaseContract {
+
+    object CancionEntry:BaseColumns{
+
+        const val TABLE_NAME="song"
+
+        const val COLUMN_NAME="name"
+        const val COLUMN_ARTISTA="artista"
+        const val COLUMN_ANIO="anio"
+        const val COLUMN_ALBUM="album"
+    }
 }
